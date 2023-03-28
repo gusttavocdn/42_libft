691463380030d7f8adeb6d5e60c880ebf4f3adfb
Gustavo da Silva <gusttavo.x.santos@gmail.com>
2023-03-27 23:17:54 -0300

    feat: add ctype fuctions

---
+++ Makefile
@@ -0,0 +1,31 @@
+NAME = libft.a
+
+SRC = ft_isalpha.c \ ft_isdigit.c
+	ft_isalnum.c \ ft_isascii.c
+	ft_isprint.c \
+
+OBJS = ${SRC:.c=.o}
+
+CC = cc
+
+FLAGS = -Wall -Wextra -Werror
+
+HEADER = libft.h
+
+all:	${NAME}
+
+${NAME}: ${OBJS} ${HEADER}
+	ar -rcs ${NAME} ${OBJS} ${HEADER}
+
+.c.o:
+	$(CC) $(FLAGS) -c $< -o ${<:.c=.o}
+
+clean:
+	${RM} ${OBJS}
+
+fclean: clean
+	${RM} ${NAME}
+
+re: fclean all
+
+.PHONY: clean all fclean re
---
+++ libft.h
@@ -0,0 +1,30 @@
+#ifndef LIBFT_H
+#define LIBFT_H
+
+// Functions from <type.h> library
+/**
+ * @brief This functions verify if a char is character from a-Z
+ */
+int ft_isalpha(int c);
+
+/**
+ * @brief This functions verify if a char is a digit from 0-9
+ */
+int ft_isdigit(int c);
+
+/**
+ * @brief This functions verify if a char is alphanumeric
+ */
+int ft_isalnum(int c);
+
+/**
+ * @brief This functions verify if a char is ascii
+ */
+int ft_isascii(int c);
+
+/**
+ * @brief This functions verify if a char is printable
+ */
+int ft_isprint(int c);
+
+#endif
