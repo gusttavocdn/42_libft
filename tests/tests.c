9b2ad52008ac76e866080a51ddbcd19fb07e48c3
Gustavo da Silva <gusttavo.x.santos@gmail.com>
2023-06-15 10:16:06 -0300

    tests: add tests folder and some test functions

---
+++ tests/tests.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gusda-si <gusda-si@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/14 19:15:29 by gusda-si          #+#    #+#             */
+/*   Updated: 2023/06/15 09:04:02 by gusda-si         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../libft.h"
+
+
+void ft_strlen_tester(void);
+
+void ft_is_tester(void);
+
+void ft_memset_tester(void);
+
+void ft_memset_test_helper(char c, size_t bytes);
+
+int main(void)
+{
+//	ft_is_tester();
+//	ft_memset_tester();
+//	ft_strlen_tester();
+	return (0);
+}
+
