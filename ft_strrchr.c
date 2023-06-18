1597e1b7ae47baf702dd875be20053cc367c2c13
Gustavo da Silva <gusttavo.x.santos@gmail.com>
2023-06-18 01:32:33 -0300

    feat: add ft_strchr strrchr and strncpm

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: gusda-si <gusda-si@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/18 00:22:37 by gusda-si          #+#    #+#             */
-/*   Updated: 2023/06/18 00:47:21 by gusda-si         ###   ########.fr       */
+/*   Updated: 2023/06/18 01:32:18 by gusda-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,15 @@
 char	*ft_strrchr(const char *str, int c)
 {
 	char	*s;
-	size_t	size;
 	size_t	str_len;
 
 	str_len = ft_strlen(str);
-	s = s + str_len;
-	size = str_len;
-	while (0 <= size)
+	s = str + str_len;
+	while (0 <= str_len)
 	{
 		if (*s == (unsigned char)c)
 			return (s);
-		size--;
+		str_len--;
 		s--;
 	}
 	return (NULL);
