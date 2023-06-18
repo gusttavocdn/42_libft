cf16ab066a042902af4b320dabda736c46b136a2
Gustavo da Silva <gusttavo.x.santos@gmail.com>
2023-06-18 02:13:50 -0300

    feat: add ft_strnstr

---
+++ ft_strnstr.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gusda-si <gusda-si@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/18 01:44:08 by gusda-si          #+#    #+#             */
+/*   Updated: 2023/06/18 02:13:38 by gusda-si         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnstr(const char *big, const char *little, size_t len)
+{
+	size_t	i;
+	size_t	l_size;
+
+	if (!big || len == 0)
+		return (NULL);
+	l_size = ft_strlen(little);
+	if (!l_size)
+		return ((char *)big);
+	while ((*big != '\0') && (i + l_size <= len))
+	{
+		if (*big == *little)
+			if (ft_strncmp(big, little, l_size) == 0)
+				return ((char *)big);
+		i++;
+		big++;
+	}
+	return (NULL);
+}
